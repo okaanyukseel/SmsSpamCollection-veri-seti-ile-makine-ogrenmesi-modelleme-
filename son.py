cdf514d78a7c93dbfb158b2028949ae49ca19dfc
Okan Yüksel <okaanyukseel@gmail.com>
2025-05-26 16:39:57 +0300

    Add files via upload

--- son.py
+++ son.py
@@ -20,7 +20,7 @@ X_train_vec = vectorizer.fit_transform(X_train)
 X_test_vec = vectorizer.transform(X_test)
 
 # 4. Model eğitimi (Lojistik Regresyon)
-model = LogisticRegression(class_weight='balanced',max_iter=1000)
+model = LogisticRegression(class_weight='balanced',max_iter=1000)           
 model.fit(X_train_vec, y_train)
 
 # 5. Tahmin ve doğruluk
